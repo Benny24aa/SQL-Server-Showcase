c09fa2e37cdb4da6be33dd3cce123471388fdfe8
Benny24aa <harleyb101010@gmail.com>
2025-03-13 16:39:47 +0000

    Creating new table for bonuses, inserting new data into table, calculating units to perform left join to allocate numerous conditions using case statement

--- Querying New Tables.sql
+++ Querying New Tables.sql
@@ -73,3 +73,43 @@ WHEN (Age > 40) THEN '10000'
 ELSE '50000'
 END
 
+
+------ 
+-- Showcase of creating new tables 
+
+CREATE TABLE northwind.dbo.bonuses
+(EmployeeID INT PRIMARY Key,
+EmployeeName varchar(50),
+SalaryRecommend INT ,
+UnitsSold INT );
+
+-- Inserting Data from other table to another
+INSERT INTO Northwind.dbo.bonuses (EmployeeID, EmployeeName, SalaryRecommend)
+SELECT Northwind.dbo.SalaryReference.EmployeeID, Northwind.dbo.SalaryReference.EmployeeName, Northwind.dbo.SalaryReference.SalaryRecommend FROM Northwind.dbo.SalaryReference
+
+-- Calculate total units in order to left join onto bonuses table
+SELECT distinct sum(Northwind.dbo.OrderFullDetail.Total) OVER(PARTITION by Northwind.dbo.OrderFullDetail.name) AS Total, Northwind.dbo.OrderFullDetail.name
+INTO Northwind.dbo.testas
+FROM Northwind.dbo.OrderFullDetail;
+
+
+SELECT Northwind.dbo.testas.name, Northwind.dbo.bonuses.EmployeeID, Northwind.dbo.bonuses.SalaryRecommend AS Salary,  Northwind.dbo.testas.Total, Northwind.dbo.SalaryReference.Age, Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total AS Ratio,
+case 
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total  > 20) then 'Salary Reducation'
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total  > 10 ) then 'Salary Maintain'
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total > 3) then 'Salary Increase'
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total > 0) then 'Praise'
+ELSE 'Null Detected'
+END AS Recommendation_Info,
+case
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total  > 20 AND Age > 60) then 'Decision 1'
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total  > 10 AND Age > 70) then 'Decision 2'
+when(Northwind.dbo.bonuses.SalaryRecommend/Northwind.dbo.testas.Total >0 ) then 'Nothing to Determined'
+ELSE 'Investigate'
+END AS Decision
+FROM Northwind.dbo.testas
+LEFT JOIN Northwind.dbo.bonuses
+ON Northwind.dbo.bonuses.EmployeeName = Northwind.dbo.testas.name
+LEFT JOIN Northwind.dbo.SalaryReference
+ON Northwind.dbo.bonuses.EmployeeID = Northwind.dbo.SalaryReference.EmployeeID
+ORDER BY (Ratio)
